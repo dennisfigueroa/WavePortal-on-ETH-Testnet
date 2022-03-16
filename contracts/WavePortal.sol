c0bee5816f0b3edd5b625a00a1dae08996760977
dennisfigueroa <denniskfig@gmail.com>
2022-03-16 11:37:24 -0500

    Implemented deply script to permanently deploy contract to local blockchain

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,16 +6,18 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves; 
-
+    mapping(address => uint) ownerWaveCount;
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
     }
     function wave() public {
         totalWaves +=1;
         console.log("%s has waved!", msg.sender);
+        ownerWaveCount[msg.sender]++;
     }
      function getTotalWaves() public view returns (uint256) {
         console.log("We have %d total waves!", totalWaves);
         return totalWaves;
      }
+
 }
