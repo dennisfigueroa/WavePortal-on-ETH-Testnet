517df663273ddba7f73a5c020bc5ee4fb848f3bd
dennisfigueroa <denniskfig@gmail.com>
2022-03-16 22:48:49 -0500

    Connecting wallet to web app

--- scripts/run.js
+++ scripts/run.js
@@ -1,42 +1,35 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
-  // Compile our contract and generate the necessary files we need to work with our contract. 
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  /* Hardhat is creating a local ethereum network for us, just for this contract, then after script completes,
-    it'll destroy that local network. Everytime you run contract, fresh blockchain. It's like refreshing your
-    local server every time so you always start from a clean slate to debug errors. 
-  */
   const waveContract = await waveContractFactory.deploy();
-  //   wait until contract is officially deployed to blockchain, constructor will run
   await waveContract.deployed();
-  // once deployed, waveContract.address will basically give us address of deployed contract. 
-  console.log("Contract deployed to:", waveContract.address);
-  console.log("Contract deployed by:", owner.address);
+  console.log("Contract addy:", waveContract.address);
 
   let waveCount;
   waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
 
-  let waveTxn = await waveContract.wave();
-  await waveTxn.wait();
+  /**
+   * Let's send a few waves!
+   */
+  let waveTxn = await waveContract.wave("A message!");
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-  waveCount = await waveContract.getTotalWaves();
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-  waveTxn = await waveContract.connect(randomPerson).wave();
-  await waveTxn.wait();
-
-waveCount = await waveContract.getTotalWaves();
-
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
-    try {
-      await main();
-      process.exit(0); // exit Node process without error
-    } catch (error) {
-      console.log(error);
-      process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
-    }
-    // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
-  };
-  
-  runMain();
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
