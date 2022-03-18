6fdf5fe41bed59651dda4281f0e72702998ae16a
dennisfigueroa <denniskfig@gmail.com>
2022-03-17 23:21:23 -0500

    updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/TBtLpG6Vj7vHhjsxUc_nOmMxmTlUrETr",
-      accounts: ["ca855269e40af8d5bdbf59da9a1da7ccf4ea0dcd682f975cf6359f8743bc2615"],
+      url: "",
+      accounts: [""],
     },
   },
 };
